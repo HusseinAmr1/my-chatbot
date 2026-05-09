bf823955f445816f5d1059a108ec4be42d066e19
Ubuntu <ubuntu@ip-172-31-37-84.eu-central-1.compute.internal>
2026-05-09 03:16:54 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, session
+import google.generativeai as genai
+import os
 
 app = Flask(__name__)
-app.secret_key = "secret123"
+app.secret_key = "supersecretkey"
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-flash-latest")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
+    # 🧠 لو مفيش history نعمله
     if "chat" not in session:
         session["chat"] = []
 
@@ -13,19 +20,18 @@ def home():
         user_message = request.form.get("message")
 
         if user_message:
-            session["chat"].append({"user": user_message})
+            try:
+                response = model.generate_content(user_message).text
+            except:
+                response = "Error getting response"
 
-            if user_message.lower() == "hello":
-                bot_response = "Hi!"
-            elif user_message.lower() == "how are you":
-                bot_response = "I'm fine 😊"
-            else:
-                bot_response = "I don't understand you."
-
-            session["chat"].append({"bot": bot_response})
+            # 💾 نحفظ الرسائل
+            session["chat"].append(("user", user_message))
+            session["chat"].append(("bot", response))
             session.modified = True
 
     return render_template("index.html", chat=session["chat"])
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
